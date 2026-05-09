c0447679972fd9bc49fe75b5d8c4057998df496f
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-08 22:15:56 -0500

    Task 2: user data entry

---
+++ JavaScript - Week #1/script.js
@@ -0,0 +1,6 @@
+// Import and activate the prompt-sync library
+const prompt = require("prompt-sync")()
+
+// User data entry
+let name = prompt("Enter your name: ");
+let age = prompt("Enter your age: ");
