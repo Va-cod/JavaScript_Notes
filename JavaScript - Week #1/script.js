da15447e783b4fe2cbfd066584dc760fab93c772
Va-cod <atenciodiazvalentina@gmail.com>
2026-05-13 21:04:04 -0500

    Delete folder

--- JavaScript - Week #1/script.js
+++
@@ -1,6 +0,0 @@
-// Import and activate the prompt-sync library
-const prompt = require("prompt-sync")()
-
-// User data entry
-let name = prompt("Enter your name: ");
-let age = prompt("Enter your age: ");
